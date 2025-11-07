3175be350fd215f3474fe8585e114419179a9fb5
Thuanny Helen <thuannyhelen@hotmail.com>
2025-11-07 10:12:37 -0300

    update settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+import tailwindcss from "@tailwindcss/vite";
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  define: {
+    "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
+  },
+  server: {
+    port: Number(process.env.VITE_PORT),
+    proxy: {
+      "/employee": {
+        target: "http://localhost",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/employee/, "/employee"),
+      },
+    },
+  },
+});
